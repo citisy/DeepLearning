372d60f39492dc9462d852a629be077e9521b1e5
citisy <citisy@163.com>
2023-07-06 19:48:43 +0800

    feat.: update

--- examples/object_detect.py
+++ examples/object_detect.py
@@ -1,5 +1,6 @@
 from .base import OdProcess
 from cv_data_parse.data_augmentation import crop, scale, geometry, pixel_perturbation, channel, RandomApply, Apply, complex
+from utils import converter
 import numpy as np
 
 
@@ -14,7 +15,7 @@ class FastererRCNN_Voc(OdProcess):
             {'score': 0.3910}
     """
 
-    def __init__(self):
+    def __init__(self, device=1):
         from object_detection.FasterRCNN import Model
 
         in_ch = 3
@@ -29,39 +30,11 @@ class FastererRCNN_Voc(OdProcess):
             model_version='FastererRCNN',
             dataset_version='Voc2012',
             input_size=input_size,
-            device=1
+            device=device
         )
 
 
-class YoloV5_Voc(OdProcess):
-    """
-    Usage:
-        .. code-block:: python
-
-            from examples.object_detect import YoloV5_Voc as Process
-
-            Process().run(max_epoch=500)
-            {'score': 0.6721}
-    """
-
-    def __init__(self):
-        from object_detection.YoloV5 import Model
-
-        in_ch = 3
-        input_size = 640
-        n_classes = 20
-
-        super().__init__(
-            model=Model(
-                n_classes,
-                in_module_config=dict(in_ch=in_ch, input_size=input_size),
-            ),
-            model_version='YoloV5',
-            dataset_version='Voc2012',
-            input_size=input_size,
-            device=1
-        )
-
+class YoloV5(OdProcess):
     def data_augment(self, ret):
         ret.update(RandomApply([geometry.HFlip()])(**ret))
         return ret
@@ -89,8 +62,8 @@ class YoloV5_Voc(OdProcess):
         ret.update(dst=self.input_size)
         ret.update(Apply([
             scale.LetterBox(),
-            pixel_perturbation.MinMax(),
-            pixel_perturbation.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            # pixel_perturbation.MinMax(),
+            # pixel_perturbation.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             channel.HWC2CHW()
         ])(**ret))
 
@@ -100,8 +73,85 @@ class YoloV5_Voc(OdProcess):
         ret.update(dst=self.input_size)
         ret.update(Apply([
             scale.LetterBox(),
-            pixel_perturbation.MinMax(),
-            pixel_perturbation.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            # pixel_perturbation.MinMax(),
+            # pixel_perturbation.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             channel.HWC2CHW()
         ])(**ret))
         return ret
+
+
+class YoloV5_Voc(YoloV5):
+    """
+    Usage:
+        .. code-block:: python
+
+            from examples.object_detect import YoloV5_Voc as Process
+
+            Process().run(max_epoch=500)
+            {'score': 0.3529}
+    """
+
+    def __init__(self, device=1):
+        from object_detection.YoloV5 import Model
+
+        in_ch = 3
+        input_size = 640
+        n_classes = 20
+
+        super().__init__(
+            model=Model(
+                n_classes,
+                in_module_config=dict(in_ch=in_ch, input_size=input_size),
+            ),
+            model_version='YoloV5',
+            dataset_version='Voc2012',
+            input_size=input_size,
+            device=device
+        )
+
+
+class YoloV5_yolov5(YoloV5):
+    def __init__(self, classes, device=1):
+        from object_detection.YoloV5 import Model
+
+        in_ch = 3
+        input_size = 640
+        n_classes = len(classes)
+
+        super().__init__(
+            model=Model(
+                n_classes,
+                in_module_config=dict(in_ch=in_ch, input_size=input_size),
+            ),
+            model_version='YoloV5',
+            dataset_version='yolov5',
+            input_size=input_size,
+            device=device
+        )
+
+    def get_train_data(self):
+        from cv_data_parse.YoloV5 import Loader, DataRegister
+
+        loader = Loader('yolov5/data_mapping')
+        data = loader(set_type=DataRegister.TRAIN, image_type=DataRegister.PATH, generator=False, sub_dir='')[0]
+
+        return data
+
+    def data_augment(self, ret):
+        image = ret['image']
+        ret['bboxes'] = converter.CoordinateConvert.mid_xywh2top_xyxy(ret['bboxes'], wh=(image.shape[1], image.shape[0]), blow_up=True)
+        return super().data_augment(ret)
+
+    def get_val_data(self):
+        from cv_data_parse.YoloV5 import Loader, DataRegister
+
+        loader = Loader('yolov5/data_mapping')
+        data = loader(set_type=DataRegister.VAL, image_type=DataRegister.PATH, generator=False, sub_dir='')[0]
+        # data = data[:20]
+
+        return data
+
+    def val_data_augment(self, ret):
+        image = ret['image']
+        ret['bboxes'] = converter.CoordinateConvert.mid_xywh2top_xyxy(ret['bboxes'], wh=(image.shape[1], image.shape[0]), blow_up=True)
+        return super().val_data_augment(ret)
