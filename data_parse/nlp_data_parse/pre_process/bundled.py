830d53f1a24d0ff458d6da722501aeda1d6bc2c0
citisy <citisy@163.com>
2023-12-20 18:19:59 +0800

    feat.: add bundled

---
+++ data_parse/nlp_data_parse/pre_process/bundled.py
@@ -0,0 +1,78 @@
+import copy
+import random
+import numpy as np
+from . import sp_token_dict
+
+
+def lower(paragraphs):
+    return [i.lower() for i in paragraphs]
+
+
+def add_token(segments, start_token=None, end_token=None):
+    if start_token is not None:
+        segments = [[start_token] + s for s in segments]
+
+    if end_token is not None:
+        segments = [s + [end_token] for s in segments]
+
+    return segments
+
+
+def random_mask(segments, word_dict, unk_tag, mask_token=sp_token_dict['mask'], non_mask_tag=-100):
+    segments = copy.deepcopy(segments)
+    vocab = list(word_dict.keys())
+    mask_tags = []
+
+    for segment in segments:
+        mask_tag = []
+        for i, word in enumerate(segment):
+            prob = random.random()
+            if prob < 0.15:  # 15% to mask
+                prob /= 0.15
+
+                # 80% to add [MASK] token
+                if prob < 0.8:
+                    segment[i] = mask_token
+
+                # 10% to change to another word
+                elif prob < 0.9:
+                    segment[i] = np.random.choice(vocab)
+
+                mask_tag.append(word_dict.get(word, unk_tag))
+
+            else:
+                mask_tag.append(non_mask_tag)
+        mask_tags.append(mask_tag)
+
+    return segments, mask_tags
+
+
+def joint(text_pairs, sep_token=sp_token_dict['sep'], keep_end=True):
+    segments = []
+    for segments_pair in text_pairs:
+        seg = []
+        for s in segments_pair:
+            seg += s + [sep_token]
+            if not keep_end:
+                seg = seg[:-1]
+        segments.append(seg)
+    return segments
+
+
+def truncate(segments, seq_len):
+    return [s[:seq_len] for s in segments]
+
+
+def pad(segments, seq_len, pad_token=sp_token_dict['pad']):
+    _segments = []
+    for s in segments:
+        pad_len = seq_len - len(s)
+        _segments.append(s + [pad_token] * pad_len)
+    return _segments
+
+
+def align(segments, seq_len, start_token=None, end_token=None, pad_token=sp_token_dict['pad']):
+    segments = add_token(segments, start_token=start_token, end_token=end_token)
+    segments = truncate(segments, seq_len=seq_len)
+    segments = pad(segments, seq_len=seq_len, pad_token=pad_token)
+    return segments
