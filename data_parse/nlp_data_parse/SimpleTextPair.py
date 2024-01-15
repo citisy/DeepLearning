dc5a5980e345068e9083ac4cf2fc0fa1bbc9ad72
citisy <citisy@163.com>
2024-01-15 18:48:39 +0800

    fix

--- data_parse/nlp_data_parse/SimpleTextPair.py
+++ data_parse/nlp_data_parse/SimpleTextPair.py
@@ -12,11 +12,11 @@ class Loader(DataLoader):
 
     default_set_type = [DataRegister.MIX]
 
-    def _call(self, **gen_kwargs):
-        with open(f'{self.data_dir}/corpus', 'r', encoding='utf8') as f:
+    def _call(self, set_type=DataRegister.TRAIN, **gen_kwargs):
+        with open(f'{self.data_dir}/{set_type.value}.txt', 'r', encoding='utf8') as f:
             gen_func = f.read().strip().split('\n')
         gen_func = enumerate(gen_func)
-        return self.gen_data(gen_func, **gen_kwargs)
+        return self.gen_data(gen_func, set_type=set_type, **gen_kwargs)
 
     def get_ret(self, obj, set_type=DataRegister.TRAIN, **kwargs) -> dict:
         i, line = obj
@@ -31,8 +31,8 @@ class Saver(DataSaver):
     def mkdirs(self, **kwargs):
         os_lib.mk_dir(self.data_dir)
 
-    def _call(self, iter_data, **gen_kwargs):
-        f = open(f'{self.data_dir}/corpus.txt', 'w', encoding='utf8')
+    def _call(self, iter_data, set_type=DataRegister.TRAIN, **gen_kwargs):
+        f = open(f'{self.data_dir}/{set_type.value}.txt', 'w', encoding='utf8')
         return self.gen_data(iter_data, f=f, **gen_kwargs)
 
     def parse_ret(self, ret, f=None, **get_kwargs):
