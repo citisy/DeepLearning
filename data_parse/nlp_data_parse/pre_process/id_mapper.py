b59577d365d2e3aca1eacdc3db186e231179c080
citisy <citisy@163.com>
2024-02-23 14:46:02 +0800

    refactor: id_mapper

--- data_parse/nlp_data_parse/pre_process/id_mapper.py
+++ data_parse/nlp_data_parse/pre_process/id_mapper.py
@@ -3,9 +3,10 @@ label text with ids, use key pairs of (acid, ), (pid, pcid) or (cid, ccid) to po
 easy to search chars from different fine-grained text type
 
 article (str): all lines fatten to a string
-segments (List[List[str]]): all lines after cut
-paragraphs (List[str]): all original lines, the lines contain the sep symbol
-chunks (List[str]): each line has the same length possibly
+paragraphs (List[str]): all original lines, each itme in list is a str line
+chunks (List[str]): each line has the same length as paragraphs as possibly, each itme in list is a str line
+segments (List[List[str]]): all lines after cut, each itme in list is a cut word list
+seg_chunks (List[List[str]]): each line has the same length as segments as possibly, each itme in list is a cut word list
 
 acid: article char id
 pid: paragraph line id
@@ -15,35 +16,83 @@ ccid: chunk char id
 """
 
 
-def get_full_id(
-        acid=None, pid_pcid=None, cid_ccid=None,
-        acid2pid_pcid=None, acid2cid_ccid=None, pid_pcid2acid=None, cid_ccid2acid=None,
-):
-    """input one id pairs of article, paragraphs or chunks, output full id pairs of others
-    if acid is given, acid2pid_pcid and acid2cid_ccid must be given
-    if pid_pcid is given, pid_pcid2acid and acid2cid_ccid must be given
-    if cid_ccid is given, cid_ccid2acid and acid2pid_pcid must be given
-    """
-    if acid:
-        pid_pcid = acid2pid_pcid[acid]
-        cid_ccid = acid2cid_ccid[acid]
+class Mapper:
+    acid2pid_pcid: dict
+    pid_pcid2acid: dict
+    acid2cid_ccid: dict
+    cid_ccid2acid: dict
+    pid_pcid2cid_ccid: dict
+    cid_ccid2pid_pcid: dict
 
-    elif pid_pcid:
-        acid = pid_pcid2acid[acid]
-        cid_ccid = acid2cid_ccid[acid]
+    def __init__(self, paragraphs=None, chunks=None):
+        self.__dict__.update(full_id_pairs(paragraphs, chunks))
 
-    elif cid_ccid:
-        acid = cid_ccid2acid[cid_ccid]
-        pid_pcid = acid2pid_pcid[acid]
+    def get_full_id(self, acid=None, pid_pcid=None, cid_ccid=None):
+        """input one id pairs of article, paragraphs or chunks, output full id pairs of others
+        if acid is given, acid2pid_pcid and acid2cid_ccid must be given
+        if pid_pcid is given, pid_pcid2acid and acid2cid_ccid must be given
+        if cid_ccid is given, cid_ccid2acid and acid2pid_pcid must be given
+        """
+        if acid:
+            pid_pcid = self.acid2pid_pcid[acid]
+            cid_ccid = self.acid2cid_ccid[acid]
 
-    return dict(
-        acid=acid,
-        pid_pcid=pid_pcid,
-        cid_ccid=cid_ccid
-    )
+        elif pid_pcid:
+            acid = self.pid_pcid2acid[pid_pcid]
+            cid_ccid = self.acid2cid_ccid[acid]
+
+        elif cid_ccid:
+            acid = self.cid_ccid2acid[cid_ccid]
+            pid_pcid = self.acid2pid_pcid[acid]
+
+        else:
+            raise ValueError('Must input one of [acid, pid_pcid, cid_ccid]')
+
+        return dict(
+            acid=acid,
+            pid_pcid=pid_pcid,
+            cid_ccid=cid_ccid
+        )
+
+    def get_full_span(self, length, acid=None, pid_pcid=None, cid_ccid=None):
+        if acid:
+            start_acid = acid
+            start_pid_pcid = self.acid2pid_pcid[start_acid]
+            start_cid_ccid = self.acid2cid_ccid[start_acid]
+
+            end_acid = acid + length
+            end_pid_pcid = self.acid2pid_pcid[end_acid]
+            end_cid_ccid = self.acid2cid_ccid[end_acid]
+
+        elif pid_pcid:
+            start_pid_pcid = pid_pcid
+            start_acid = self.pid_pcid2acid[start_pid_pcid]
+            start_cid_ccid = self.acid2cid_ccid[start_acid]
+
+            end_pid_pcid = (pid_pcid[0], pid_pcid[1] + length)
+            end_acid = self.pid_pcid2acid[end_pid_pcid]
+            end_cid_ccid = self.acid2cid_ccid[end_acid]
+
+        elif cid_ccid:
+            start_cid_ccid = cid_ccid
+            start_acid = self.cid_ccid2acid[start_cid_ccid]
+            start_pid_pcid = self.acid2pid_pcid[start_acid]
+
+            end_cid_ccid = (cid_ccid[0], cid_ccid[1] + length)
+            end_acid = self.cid_ccid2acid[end_cid_ccid]
+            end_pid_pcid = self.acid2pid_pcid[end_acid]
+
+        else:
+            raise ValueError('Must input one of [acid, pid_pcid, cid_ccid]')
+
+        return dict(
+            acid_span=(start_acid, end_acid),
+            pid_pcid_span=(start_pid_pcid, end_pid_pcid),
+            cid_ccid_span=(start_cid_ccid, end_cid_ccid)
+        )
 
 
-def full_id_pairs(paragraphs, chunks):
+def full_id_pairs(paragraphs=None, chunks=None):
     """
     Usages:
         >>> full_id_pairs(['abcdefghijklmn'], ['abcde', 'fghij', 'klmn'])
@@ -55,9 +104,12 @@ def full_id_pairs(paragraphs, chunks):
         'cid_ccid2pid_pcid': {(0, 0): (0, 0), (0, 1): (0, 1), (0, 2): (0, 2), (0, 3): (0, 3), (0, 4): (0, 4), (1, 0): (0, 5), (1, 1): (0, 6), (1, 2): (0, 7), (1, 3): (0, 8), (1, 4): (0, 9), (2, 0): (0, 10), (2, 1): (0, 11), (2, 2): (0, 12), (2, 3): (0, 13)}}
     """
     r = dict()
-    r.update(article_paragraphs_id_pairs(paragraphs))
-    r.update(article_chunks_id_pairs(chunks))
-    r.update(paragraphs_chunks_id_pairs(paragraphs, chunks, r['acid2pid_pcid'], r['acid2cid_ccid']))
+    if paragraphs:
+        r.update(article_paragraphs_id_pairs(paragraphs))
+    if chunks:
+        r.update(article_chunks_id_pairs(chunks))
+    if paragraphs and chunks:
+        r.update(paragraphs_chunks_id_pairs(paragraphs, chunks, r['acid2pid_pcid'], r['acid2cid_ccid']))
 
     return r
 
