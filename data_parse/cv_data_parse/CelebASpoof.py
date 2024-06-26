0bab50bca818dc40e8f0dfd210f019f1469a5477
citisy <citisy@163.com>
2024-06-26 12:00:49 +0800

    feat.: make data_parse submodule

--- data_parse/cv_data_parse/CelebASpoof.py
+++
@@ -1,6 +0,0 @@
-from .base import DataRegister, DataLoader, DataSaver, get_image
-
-
-class CelebASpoofLoader(DataLoader):
-    """http://mmlab.ie.cuhk.edu.hk/projects/CelebA/CelebA_Spoof.html
-    """
